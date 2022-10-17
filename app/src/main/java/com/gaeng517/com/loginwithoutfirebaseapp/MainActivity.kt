6fb8b31b768b9d373f1f8f33cd7376bd8eab1b23
JIKYUNGHEE <khji@wemade.com>
2022-10-17 20:41:58 +0900

    - First Commit - Add .gitignore

---
+++ app/src/main/java/com/gaeng517/com/loginwithoutfirebaseapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gaeng517.com.loginwithoutfirebaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
